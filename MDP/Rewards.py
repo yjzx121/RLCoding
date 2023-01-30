33f520bbda745655338c8a7d4221bcf461b18a8b
yjzx121 <724417336@qq.com>
2023-01-30 19:15:18 +0800

    start

---
+++ MDP/Rewards.py
@@ -0,0 +1,115 @@
+import numpy as np
+
+
+np.random.seed(0)
+# 定义状态转移概率矩阵P
+P = [
+    [0.9, 0.1, 0.0, 0.0, 0.0, 0.0],
+    [0.5, 0.0, 0.5, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.6, 0.0, 0.4],
+    [0.0, 0.0, 0.0, 0.0, 0.3, 0.7],
+    [0.0, 0.2, 0.3, 0.5, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+]
+P = np.array(P)
+
+rewards = [-1, -2, -2, 10, 1, 0]  # 定义奖励函数
+gamma = 0.5  # 定义折扣因子
+
+
+# 给定一条序列,计算从某个索引（起始状态）开始到序列最后（终止状态）得到的回报
+def compute_return(start_index, chain, gamma):
+    G = 0
+    for i in reversed(range(start_index, len(chain))):
+        G = gamma * G + rewards[chain[i] - 1]
+    return G
+
+
+# 一个状态序列,s1-s2-s3-s6
+chain = [1, 2, 3, 6]
+start_index = 0
+G = compute_return(start_index, chain, gamma)
+print("根据本序列计算得到回报为：%s。" % G)
+
+
+def compute(P, rewards, gamma, states_num):
+    '''利用贝尔曼方程的矩阵形式计算解析解, states_num是MRP的状态数'''
+    rewards = np.array(rewards).reshape((-1, 1))
+    value = np.dot(np.linalg.inv(np.eye(states_num, states_num) - gamma * P), rewards)
+    return value
+
+
+V = compute(P, rewards, gamma, 6)
+print("MRP中每个状态价值分别为\n", V)
+
+
+S = ["s1", "s2", "s3", "s4", "s5"]  # 状态集合
+A = ["保持s1", "前往s1", "前往s2", "前往s3", "前往s4", "前往s5", "概率前往"]  # 动作集合
+# 状态转移函数
+P = {
+    "s1-保持s1-s1": 1.0,
+    "s1-前往s2-s2": 1.0,
+    "s2-前往s1-s1": 1.0,
+    "s2-前往s3-s3": 1.0,
+    "s3-前往s4-s4": 1.0,
+    "s3-前往s5-s5": 1.0,
+    "s4-前往s5-s5": 1.0,
+    "s4-概率前往-s2": 0.2,
+    "s4-概率前往-s3": 0.4,
+    "s4-概率前往-s4": 0.4,
+}
+# 奖励函数
+R = {
+    "s1-保持s1": -1,
+    "s1-前往s2": 0,
+    "s2-前往s1": -1,
+    "s2-前往s3": -2,
+    "s3-前往s4": -2,
+    "s3-前往s5": 0,
+    "s4-前往s5": 10,
+    "s4-概率前往": 1,
+}
+
+gamma = 0.5
+MDP = (S, A, P, R, gamma)
+# 策略1,随机策略
+Pi_1 = {
+    "s1-保持s1": 0.5,
+    "s1-前往s2": 0.5,
+    "s2-前往s1": 0.5,
+    "s2-前往s3": 0.5,
+    "s3-前往s4": 0.5,
+    "s3-前往s5": 0.5,
+    "s4-前往s5": 0.5,
+    "s4-概率前往": 0.5,
+}
+# 策略2
+Pi_2 = {
+    "s1-保持s1": 0.6,
+    "s1-前往s2": 0.4,
+    "s2-前往s1": 0.3,
+    "s2-前往s3": 0.7,
+    "s3-前往s4": 0.5,
+    "s3-前往s5": 0.5,
+    "s4-前往s5": 0.1,
+    "s4-概率前往": 0.9,
+}
+
+
+# 把输入的两个字符串通过“-”连接,便于使用上述定义的P、R变量
+def join(str1, str2):
+    return str1 + '-' + str2
+
+
+P_from_mdp_to_mrp = [
+    [0.5, 0.5, 0.0, 0.0, 0.0],
+    [0.5, 0.0, 0.5, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.5, 0.5],
+    [0.0, 0.1, 0.2, 0.2, 0.5],
+    [0.0, 0.0, 0.0, 0.0, 1.0],
+]
+P_from_mdp_to_mrp = np.array(P_from_mdp_to_mrp)
+R_from_mdp_to_mrp = [-0.5, -1.5, -1.0, 5.5, 0]
+
+V = compute(P_from_mdp_to_mrp, R_from_mdp_to_mrp, gamma, 5)
+print("MDP中每个状态价值分别为\n", V)
